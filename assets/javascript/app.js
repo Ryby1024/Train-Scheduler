62bbf3f38bc3d7bdbd1bad5069d7ed15c1b85446
Roy Davis <myroy1024@yahoo.com>
2019-10-28 16:43:36 -0400

    Got all the logic working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,29 +12,81 @@ let firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
-var date = new Date();
-var hour = date.getHours();
-var min = date.getMinutes();
 
-console.log(hour, min)
-$("#add-train").on("click", function (event) {
+
+
+$("#submitBtn").on("click", function (event) {
 
     event.preventDefault();
 
+    // Gathering the users input values.
+    var trainName = $("#train-name").val().trim();
+    var trainDestination = $("#destination").val().trim();
+    var trainFirstTime = $("#first-train-time").val().trim();
+    var trainFreq = $("#train-freq").val().trim();
 
-    var name = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
-    var firstTime = parseInt($("#first-train-time").val().trim());
-    var trainFreq = parseInt($("#train-freq").val().trim());
+    console.log(name);
+    console.log(destination);
+    console.log(trainFirstTime);
+    console.log(trainFreq);
 
-    database.ref().set({
-        name: "train-name",
-        destination: "destination",
-        firstTime: "train-first-time",
-        trainFreq: "train-freq"
+    // Pushing the info gathered from the user to Firebase.
+    database.ref().push({
+        name: trainName,
+        destination: trainDestination,
+        firstTime: trainFirstTime,
+        trainFreq: trainFreq,
+
     })
-  });
+    // Clearing the input fields after a submission.
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#train-freq").val("");
+});
 
-  database.ref().on("value", function(snapshot){
-      
-  })
+database.ref().on("child_added", function (snapshot) {
+
+    let sv = snapshot.val();
+    // Using moment js to convert the train times to 1 year ago.
+    let convertedTime = moment(sv.trainFirstTime, "HH:mm").subtract(1, "years");
+    console.log(convertedTime);
+    let currentTime = moment();
+
+    // Setting the difference between now and the converted time to a variable.
+    let timeDiff = moment().diff(convertedTime, "minutes");
+
+    // Finding out the time remaining for the train to arrive.
+    let timeRemaind = timeDiff % sv.trainFreq;
+
+    let minAway = sv.trainFreq - timeRemaind;
+    console.log(minAway);
+
+    let nxtArrive = moment().add(minAway, "minutes");
+
+    // Converting the military time to standard time to display on the page
+    let newTrainTimeForm = moment(nxtArrive).format("hh:mm A");
+
+    console.log(moment(currentTime).format("HH:mm"));
+
+    // Using JQuery to create the table with the info gathered.
+
+    let tRow = $("<tr>");
+    let train = $("<td>").text(sv.name);
+    let trainDest = $("<td>").text(sv.destination);
+    let freq = $("<td>").text(sv.trainFreq);
+    let minutesAway = $("<td>").text(minAway);
+    let nextArrive = $("<td>").text(newTrainTimeForm);
+
+
+    
+    tRow.append(train);
+    tRow.append(trainDest);
+    tRow.append(freq);
+    tRow.append(nextArrive);
+    tRow.append(minutesAway);
+
+    // Appending the table with the Train info to the HTML
+    tRow.appendTo(".tbody")
+
+})
